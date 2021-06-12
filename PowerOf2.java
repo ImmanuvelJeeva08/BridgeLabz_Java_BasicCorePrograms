cf639d15986ba1a3b0191ea8996f2ae167637ae5
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-06-12 13:39:53 -0700

    Power of 2 program

---
+++ PowerOf2.java
@@ -0,0 +1,27 @@
+/**
+  * Get input N prints table of power of 2 that are less then are equal to 2^N
+  */
+
+import java.util.*;
+class PowerOf2 {
+	public static void main(String[] args) {
+
+		// Get input from user
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Power of 2");
+		int N=sc.nextInt();	
+		int sum=1;
+
+		while (N >= 1) {  // print powers of 2^N
+			sum=sum*2;
+			System.out.println("Table of the power of 2 :");
+			System.out.println(sum);
+			if (sum >= N) {  // check power of 2^N is >= to N
+   			break;
+			}
+			N--;
+		}
+		System.out.println("sum = "+sum);
+	}
+}			
