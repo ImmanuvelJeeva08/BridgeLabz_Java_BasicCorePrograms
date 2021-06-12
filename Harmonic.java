e3e9a391782990a154c9d50c69154271eb8052d6
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-06-12 13:36:21 -0700

    Harmonic program

---
+++ Harmonic.java
@@ -0,0 +1,23 @@
+/**
+  * Prints and compute upto 1/1+1/2+.....+1/N
+  */
+
+import java.util.*;
+class Harmonic {
+        public static void main(String[] args) {
+
+		// Get input N from the user
+
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Enter the harmoni value");
+                int hv=sc.nextInt();
+		double sum=0;
+	
+		// Print value by using for loop	
+
+		for (int i=1; i<=hv; i++) {
+			sum=sum+(1.0/i);
+		}
+		System.out.println("sum = "+sum);
+	}
+}
